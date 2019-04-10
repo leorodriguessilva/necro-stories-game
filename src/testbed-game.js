0009ec6496bde17679ee48bda57c341f363716df
unknown <leonardo_faust@hotmail.com>
2019-04-09 23:18:24 -0300

    added necromancer and skeleton to the testbed scene, to see their behaviour while testing the game

--- src/testbed-game.js
+++ src/testbed-game.js
@@ -20,28 +20,34 @@ let game = new Phaser.Game(config);
 
 let statsReader = new StatsReader(StatsReaderMode.DEBUG_MODE);
 
-let necromancer = new Necromancer(100, 400, statsReader, [], []);
+let skeleton = new Skeleton(100, 400, statsReader, [], []);
+
+let necromancer = new Necromancer(300, 400, statsReader, [], []);
 
 function preload ()
 {
+    skeleton.preload(this.load);
     necromancer.preload(this.load);
 }
 
 function create ()
 {
+    skeleton.create(this.physics, this.anims, []);
+
     necromancer.create(this.physics, this.anims, []);
 
     var leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT, true, true);
     var rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT, true, true);
 
-    var walkLeftInputHandler = new WalkLeftInputHandler(leftKey, necromancer, 'necromancer-left')
-    var walkRightInputHandler = new WalkRightInputHandler(rightKey, necromancer, 'necromancer-right')
+    skeleton.addInputHandler(new WalkLeftInputHandler(leftKey, skeleton));
+    skeleton.addInputHandler(new WalkRightInputHandler(rightKey, skeleton));
 
-    necromancer.addInputHandler(walkLeftInputHandler);
-    necromancer.addInputHandler(walkRightInputHandler);
+    necromancer.addInputHandler(new WalkLeftInputHandler(leftKey, necromancer));
+    necromancer.addInputHandler(new WalkRightInputHandler(rightKey, necromancer));
 }
 
 function update ()
 {
+    skeleton.update();
     necromancer.update();
 }

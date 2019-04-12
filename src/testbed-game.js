eda84c154ca4ec99254387b4390c94bfa435bcf6
unknown <leonardo_faust@hotmail.com>
2019-04-11 23:20:48 -0300

    added ghost colision handler with stars example to in the testbed game scene

--- src/testbed-game.js
+++ src/testbed-game.js
@@ -20,6 +20,8 @@ let game = new Phaser.Game(config);
 
 let platforms;
 
+let stars;
+
 let statsReader = new StatsReader(StatsReaderMode.DEBUG_MODE);
 
 let skeleton = new Skeleton(100, 400, statsReader, [], []);
@@ -30,6 +32,7 @@ function preload ()
 {
     skeleton.preload(this.load);
     necromancer.preload(this.load);
+    this.load.image('star', 'assets/star.png');
     this.load.image('ground', 'assets/platform.png');
 }
 
@@ -43,13 +46,32 @@ function create ()
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
+    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate(function (child) {
+        //  Give each star a slightly different bounce
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
+    var starsColliderWrapper = new ColliderWrapper(stars); 
+
+    var ghostColisionHandler = new GhostColisionHandler(this.physics, starsColliderWrapper, function (stats) {
+        stats.setMoveSpeedFactor = 12;
+    });
+
     var platformsColliderWrapper = new ColliderWrapper(platforms); 
 
     var basicColisionHandler = new BasicColisionHandler(this.physics, platformsColliderWrapper);
 
     skeleton.create(this.physics, this.anims, [ basicColisionHandler ]);
 
-    necromancer.create(this.physics, this.anims, [ basicColisionHandler ]);
+    necromancer.create(this.physics, this.anims, [ basicColisionHandler, ghostColisionHandler ]);
 
     var leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT, true, true);
     var rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT, true, true);
@@ -59,6 +81,9 @@ function create ()
 
     necromancer.addInputHandler(new WalkLeftInputHandler(leftKey, necromancer));
     necromancer.addInputHandler(new WalkRightInputHandler(rightKey, necromancer));
+
+
+    this.physics.add.collider(stars, platforms);
 }
 
 function update ()

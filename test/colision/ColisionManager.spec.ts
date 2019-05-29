b665afe2dd5060b7cd9eddc643ab813ca4d11596
unknown <leonardo_faust@hotmail.com>
2019-05-28 21:11:56 -0300

    adding more tests to the colision manager

--- test/colision/ColisionManager.spec.ts
+++ test/colision/ColisionManager.spec.ts
@@ -100,20 +100,136 @@ describe("testing colisions with one or multiply colision detections, to validat
         expect(fakeColision2.onColisionHappen).toBeCalled();
     });
     
-    test("when multiply colisions is added to handle, and no colisions happen  callback should be called", () => {
+    test("when multiply colisions is added to handle, and no colisions happen no callback should be called", () => {
         const characterCollider = new FakeCharacterCollider();
         const obstacleCollider = new FakeObstacleCollider();
-        const fakeColision1 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        const fakeColision1 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
         fakeColision1.onColisionHappen = jest.fn();
-        const fakeColision2 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        const fakeColision2 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
         fakeColision2.onColisionHappen = jest.fn();
         const fakeColisionWatcher = new FakeColisionWatcher();
         const colisionManager = new ColisionManager(fakeColisionWatcher);
         colisionManager.addColisionToHandle(fakeColision1, ColisionType.COLLIDE);
         colisionManager.addColisionToHandle(fakeColision2, ColisionType.COLLIDE);
         fakeColisionWatcher.simulateColisions();
-        expect(fakeColision1.onColisionHappen).toBeCalled();
-        expect(fakeColision2.onColisionHappen).toBeCalled();
+        expect(fakeColision1.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeColision2.onColisionHappen).toBeCalledTimes(0);
+    });
+    
+    test("when multiply colisions and one overlaping is added to handle, and an overlap happen just the overlap callback should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeColision1 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeColision1.onColisionHappen = jest.fn();
+        const fakeColision2 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeColision2.onColisionHappen = jest.fn();
+        const fakeOverlap = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeColision1, ColisionType.COLLIDE);
+        colisionManager.addColisionToHandle(fakeColision2, ColisionType.COLLIDE);
+        colisionManager.addColisionToHandle(fakeOverlap, ColisionType.OVERLAP);
+        fakeColisionWatcher.simulateColisions();
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeColision1.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeColision2.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeOverlap.onColisionHappen).toBeCalled();
+    });
+    
+    test("when multiply overlaping is added to handle, and an overlap happen just the one overlap callback should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeOverlap1 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap1.onColisionHappen = jest.fn();
+        const fakeOverlap2 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap2.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeOverlap1, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeOverlap2, ColisionType.OVERLAP);
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeOverlap1.onColisionHappen).toBeCalled();
+        expect(fakeOverlap2.onColisionHappen).toBeCalledTimes(0);
+    });
+    
+    test("when multiply overlaping is added to handle, and all overlaps happens just all overlaps callbacks should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeOverlap1 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap1.onColisionHappen = jest.fn();
+        const fakeOverlap2 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap2.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeOverlap1, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeOverlap2, ColisionType.OVERLAP);
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeOverlap1.onColisionHappen).toBeCalled();
+        expect(fakeOverlap2.onColisionHappen).toBeCalled();
+    });
+
+    test("when multiply overlaping and one colision is added to handle, and all overlaps happens just all overlaps callbacks should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeOverlap1 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap1.onColisionHappen = jest.fn();
+        const fakeOverlap2 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap2.onColisionHappen = jest.fn();
+        const fakeColision = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeColision.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeOverlap1, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeOverlap2, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeColision, ColisionType.COLLIDE);
+        fakeColisionWatcher.simulateColisions();
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeOverlap1.onColisionHappen).toBeCalled();
+        expect(fakeOverlap2.onColisionHappen).toBeCalled();
+        expect(fakeColision.onColisionHappen).toBeCalledTimes(0);
+    });
+
+    test("when multiply overlaping and one colision is added to handle, and one colision happen just one colision callback should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeOverlap1 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap1.onColisionHappen = jest.fn();
+        const fakeOverlap2 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap2.onColisionHappen = jest.fn();
+        const fakeColision = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeColision.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeOverlap1, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeOverlap2, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeColision, ColisionType.COLLIDE);
+        fakeColisionWatcher.simulateColisions();
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeOverlap1.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeOverlap2.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeColision.onColisionHappen).toBeCalled();
+    });
+
+    test("when multiply overlaping and one colision is added to handle, and one colision and a overlap happens just one colision and overlap callback should be called", () => {
+        const characterCollider = new FakeCharacterCollider();
+        const obstacleCollider = new FakeObstacleCollider();
+        const fakeOverlap1 = new FakeColision<CharacterStats, ObstacleStats>(NOT_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap1.onColisionHappen = jest.fn();
+        const fakeOverlap2 = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeOverlap2.onColisionHappen = jest.fn();
+        const fakeColision = new FakeColision<CharacterStats, ObstacleStats>(HAS_COLLIDED, characterCollider, obstacleCollider);
+        fakeColision.onColisionHappen = jest.fn();
+        const fakeColisionWatcher = new FakeColisionWatcher();
+        const colisionManager = new ColisionManager(fakeColisionWatcher);
+        colisionManager.addColisionToHandle(fakeOverlap1, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeOverlap2, ColisionType.OVERLAP);
+        colisionManager.addColisionToHandle(fakeColision, ColisionType.COLLIDE);
+        fakeColisionWatcher.simulateColisions();
+        fakeColisionWatcher.simulateOverlaping();
+        expect(fakeOverlap1.onColisionHappen).toBeCalledTimes(0);
+        expect(fakeOverlap2.onColisionHappen).toBeCalled();
+        expect(fakeColision.onColisionHappen).toBeCalled();
     });
 
 });
